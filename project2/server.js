33b1a629472d5aeaa2e36ac0a934cd8cad037a99
Aldair Rutte <xxpiltisxx@gmail.com>
2026-05-23 17:10:53 -0500

    The project 1 and project 2 was added

---
+++ project2/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express()
+
+
+app.use((req, res) => {
+    res.send("Hello world")
+})
+
+
+
+app.listen(8080, () => {
+    console.log('Server Staring: localhost:8080')
+})
